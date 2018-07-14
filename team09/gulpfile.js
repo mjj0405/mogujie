9ae6df468514d1d1c12a476d8feffa9d2a979a2a
mjj <m18518503643@163.com>
2018-07-14 10:24:02 +0800

    服务搭建完成

---
+++ team09/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var server = require('gulp-webserver');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+gulp.task('server', function() {
+    gulp.src('src')
+        .pipe(server({
+            port: 8888,
+            middleware: function(req, res, next) {
+                var pathname = url.parse(req.url).pathname;
+                if (pathname === '/favicon.ico') {
+                    return false;
+                }
+                pathname = pathname === '/' ? 'index.html' : pathname;
+                res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)));
+            }
+        }))
+})
